ae660c80dde035a492c15acd875f1aedba3dcec3
Nadun Sanjeevana <108867149+NadunSanjeevana@users.noreply.github.com>
2023-09-02 23:31:55 +0530

    edit search table

--- src/layouts/dashboard/components/Projects/data/index.js
+++ src/layouts/dashboard/components/Projects/data/index.js
@@ -16,61 +16,70 @@ Coded by www.creative-tim.com
 */
 
 // @mui material components
-import Tooltip from "@mui/material/Tooltip";
-import MDBox from "components/MDBox";
+//import Tooltip from "@mui/material/Tooltip";
+//import MDBox from "components/MDBox";
 import MDTypography from "components/MDTypography";
-import MDAvatar from "components/MDAvatar";
+//import MDAvatar from "components/MDAvatar";
 
 // Images
 
 export default function data() {
-  const avatars = (members) =>
-    members.map(([image, name]) => (
-      <Tooltip key={name} title={name} placeholder="bottom">
-        <MDAvatar
-          src={image}
-          alt="name"
-          size="xs"
-          sx={{
-            border: ({ borders: { borderWidth }, palette: { white } }) =>
-              `${borderWidth[2]} solid ${white.main}`,
-            cursor: "pointer",
-            position: "relative",
+  // const avatars = (members) =>
+  //   members.map(([image, name]) => (
+  //     <Tooltip key={name} title={name} placeholder="bottom">
+  //       <MDAvatar
+  //         src={image}
+  //         alt="name"
+  //         size="xs"
+  //         sx={{
+  //           border: ({ borders: { borderWidth }, palette: { white } }) =>
+  //             `${borderWidth[2]} solid ${white.main}`,
+  //           cursor: "pointer",
+  //           position: "relative",
 
-            "&:not(:first-of-type)": {
-              ml: -1.25,
-            },
+  //           "&:not(:first-of-type)": {
+  //             ml: -1.25,
+  //           },
 
-            "&:hover, &:focus": {
-              zIndex: "10",
-            },
-          }}
-        />
-      </Tooltip>
-    ));
+  //           "&:hover, &:focus": {
+  //             zIndex: "10",
+  //           },
+  //         }}
+  //       />
+  //     </Tooltip>
+  //   ));
 
   return {
     columns: [
-      { Header: "#", accessor: "companies", width: "5%", align: "left" },
-      { Header: "Title", accessor: "members", width: "10%", align: "left" },
-      { Header: "Source", accessor: "budget", align: "center" },
-      { Header: "City", accessor: "completion", align: "center" },
-      { Header: "Date", accessor: "a", align: "center" },
-      { Header: "Address", accessor: "b", align: "center" },
-      { Header: "PhoneNumber", accessor: "c", align: "center" },
-      { Header: "Price", accessor: "d", align: "center" },
+      { Header: "#", accessor: "index", width: "5%", align: "left" },
+      { Header: "Title", accessor: "title", width: "10%", align: "left" },
+      { Header: "Source", accessor: "source", align: "center" },
+      { Header: "City", accessor: "city", align: "center" },
+      { Header: "Date", accessor: "date", align: "center" },
+      { Header: "Address", accessor: "address", align: "center" },
+      { Header: "PhoneNumber", accessor: "phoneNumber", align: "center" },
+      { Header: "Price", accessor: "price", align: "center" },
     ],
 
     rows: [
       {
-        companies: 1,
-        members: "#8976",
-        budget: (
-          <MDTypography variant="caption" color="text" fontWeight="medium">
-            $14,000
+        index: 1,
+        title: "#8976",
+        source: "Sunday Observer",
+        city: "Badulla",
+        //date: "2023/02/14",
+        date: (
+          <MDTypography variant="caption" color="secondary">
+            2023/08/29
           </MDTypography>
         ),
-        completion: "Badulla",
+        address: "129 3rd, Cross Street",
+        phoneNumber: "078475891",
+        price: (
+          <MDTypography variant="caption" color="text" fontWeight="medium">
+            Rs.100,000
+          </MDTypography>
+        ),
       },
     ],
   };
