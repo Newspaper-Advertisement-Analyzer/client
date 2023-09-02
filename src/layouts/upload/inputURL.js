08a5b4fe8cbba778de91bc572e9ea4089c45eb24
Chanuka Lakshan Chandra Yapa <118423471+Chanuka-ChandraYapa@users.noreply.github.com>
2023-09-02 09:44:34 +0530

    changed upload

--- src/layouts/upload/inputURL.js
+++ src/layouts/upload/inputURL.js
@@ -1,13 +1,27 @@
-import React, { useState } from "react";
-import Button from "@mui/material/Button";
-import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
+import React, { useState, useEffect } from "react";
 import LinearProgress from "@mui/material/LinearProgress";
+import MDBox from "components/MDBox";
+import MDButton from "components/MDButton";
+import MDTypography from "components/MDTypography";
+import MDInput from "components/MDInput";
+import { Paper } from "@mui/material";
 
 function InputURL() {
   const [inputUrl, setInputUrl] = useState("");
   const [backendResponse, setBackendResponse] = useState([]);
   const [loading, setLoading] = useState(false);
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (currentIndex < backendResponse.length - 1) {
+        setCurrentIndex(currentIndex + 1);
+      }
+    }, 1000); // Adjust the delay (in milliseconds) as needed for your desired animation speed
+
+    return () => clearTimeout(timer);
+  }, [currentIndex, backendResponse]);
 
   const handleUrlSubmit = (e) => {
     e.preventDefault();
@@ -38,53 +52,123 @@ function InputURL() {
 
   return (
     <div>
-      <form onSubmit={handleUrlSubmit}>
-        <label>
-          <p>Enter URL to be analyzed:</p>
-          <Box
-            sx={{
-              width: 500,
-              maxWidth: "100%",
-            }}
-          >
-            <TextField
-              fullWidth
-              label="Enter URL"
-              value={inputUrl}
-              onChange={(e) => setInputUrl(e.target.value)}
-              placeholder="https://example.com"
-              required
-            />
-          </Box>
-        </label>
-        <Button variant="contained" type="submit">
-          Analyze
-        </Button>
-      </form>
-      <br />
+      <MDBox
+        mt={5}
+        mb={3}
+        alignItems="center"
+        sx={{
+          display: "flex",
+          flexDirection: "column", // To center horizontally and vertically
+          justifyContent: "center", // To center horizontally and vertically
+          alignItems: "center",
+          maxWidth: "100%",
+        }}
+      >
+        <form onSubmit={handleUrlSubmit}>
+          <label>
+            <MDTypography
+              textAlign="center"
+              variant="h4"
+              fontWeight="medium"
+              color="black"
+              mt={1}
+              mb={5}
+            >
+              Enter URL to be analyzed
+            </MDTypography>
+            <MDBox
+              alignItems="center"
+              sx={{
+                width: 500,
+                maxWidth: "100%",
+              }}
+            >
+              <MDBox mb={2} alignItems="center">
+                <MDInput
+                  alignItems="center"
+                  type="url"
+                  label="Enter URL"
+                  fullWidth
+                  value={inputUrl}
+                  onChange={(e) => setInputUrl(e.target.value)}
+                  placeholder="https://example.com"
+                  required
+                />
+              </MDBox>
+            </MDBox>
+          </label>
+          <MDBox mt={5} mb={3}>
+            <MDButton color="primary" type="submit">
+              Analyze
+            </MDButton>
+          </MDBox>
+        </form>
 
-      {loading && (
-        <div>
-          <p>Analyzing...</p>
-          <LinearProgress />
-        </div>
-      )}
-
-      {backendResponse.length > 0 && (
-        <div>
-          <p>Results:</p>
+        {loading && (
           <div>
-            <p>Title: {backendResponse[0]}</p>
-            <p>Text: {backendResponse[1]}</p>
-            <p>Summary: {backendResponse[2]}</p>
-            <p>Keywords: {backendResponse[3].join(", ")}</p>
-            <p>Category: {backendResponse[4]}</p>
-            <p>Price: {backendResponse[5]}</p>
-            <p>Contact: {backendResponse[6]}</p>
-            <p>Locations: {backendResponse[7].join(", ")}</p>
+            <p>Analyzing...</p>
+            <LinearProgress />
           </div>
-        </div>
-      )}
+        )}
+
+        {backendResponse.length > 0 && (
+          // <div>
+          //   <p>Results:</p>
+          //   <div>
+          //     <p>Title: {backendResponse[0]}</p>
+          //     <p>Text: {backendResponse[1]}</p>
+          //     <p>Summary: {backendResponse[2]}</p>
+          //     <p>Keywords: {backendResponse[3].join(", ")}</p>
+          //     <p>Category: {backendResponse[4]}</p>
+          //     <p>Price: {backendResponse[5]}</p>
+          //     <p>Contact: {backendResponse[6]}</p>
+          //     <p>Locations: {backendResponse[7].join(", ")}</p>
+          //   </div>
+          // </div>
+          <MDBox mt={5} mb={3} alignItems="center" fullWidth>
+            <Paper elevation={3} style={{ padding: "16px", marginBottom: "16px" }}>
+              <MDTypography variant="body1">Title: {backendResponse[0]}</MDTypography>
+            </Paper>
+            <Paper elevation={3} style={{ padding: "16px", marginBottom: "16px" }}>
+              <MDTypography variant="body1">Text: {backendResponse[1]}</MDTypography>
+            </Paper>
+            <Paper elevation={3} style={{ padding: "16px", marginBottom: "16px" }}>
+              <MDTypography variant="body1">Summary: {backendResponse[2]}</MDTypography>
+            </Paper>
+            <Paper elevation={3} style={{ padding: "16px", marginBottom: "16px" }}>
+              <MDTypography variant="body1">Keywords: {backendResponse[3].join(", ")}</MDTypography>
+            </Paper>
+            <Paper elevation={3} style={{ padding: "16px", marginBottom: "16px" }}>
+              <MDTypography variant="body1">Category: {backendResponse[4]}</MDTypography>
+            </Paper>
+            <Paper elevation={3} style={{ padding: "16px", marginBottom: "16px" }}>
+              <MDTypography variant="body1">Price: {backendResponse[5]}</MDTypography>
+            </Paper>
+            <Paper elevation={3} style={{ padding: "16px", marginBottom: "16px" }}>
+              <MDTypography variant="body1">Contact: {backendResponse[6]}</MDTypography>
+            </Paper>
+            <Paper elevation={3} style={{ padding: "16px" }}>
+              <MDTypography variant="body1">
+                Locations: {backendResponse[7].join(", ")}
+              </MDTypography>
+            </Paper>
+            {/* {backendResponse.slice(0, currentIndex + 1).map((item, index) => (
+              <Paper
+                elevation={3}
+                style={{
+                  padding: "16px",
+                  marginBottom: "16px",
+                  transition: "transform 0.5s ease-in-out",
+                  transform: `translateY(${index === currentIndex ? 0 : 100}%)`,
+                }}
+                key={index}
+              >
+                <MDTypography variant="body1">{item}</MDTypography>
+              </Paper>
+            ))} */}
+          </MDBox>
+        )}
+      </MDBox>
     </div>
   );
 }
--- src/layouts/upload/tabs.js
+++ src/layouts/upload/tabs.js
@@ -2,6 +2,9 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
+import Icon from "@mui/material/Icon";
+import { useState, useEffect } from "react";
+import breakpoints from "assets/theme/base/breakpoints";
 // import InputURL from "../pages/inputURL";
 // import Input_Image_URL from "../pages/inputIMGURL";
 // import LineChartExample from "../pages/charts";
@@ -12,6 +15,26 @@ import InputURL from "./inputURL";
 
 export default function CenteredTabs() {
   const [value, setValue] = React.useState(0);
+  const [tabsOrientation, setTabsOrientation] = useState("horizontal");
+  useEffect(() => {
+    // A function that sets the orientation state of the tabs.
+    function handleTabsOrientation() {
+      return window.innerWidth < breakpoints.values.sm
+        ? setTabsOrientation("vertical")
+        : setTabsOrientation("horizontal");
+    }
+
+    /** 
+     The event listener that's calling the handleTabsOrientation function when resizing the window.
+    */
+    window.addEventListener("resize", handleTabsOrientation);
+
+    // Call the handleTabsOrientation function to set the state with the initial value.
+    handleTabsOrientation();
+
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleTabsOrientation);
+  }, [tabsOrientation]);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -19,22 +42,41 @@ export default function CenteredTabs() {
 
   return (
     <Box sx={{ width: "100%", bgcolor: "background.paper" }}>
-      <Tabs value={value} onChange={handleChange} centered>
-        <Tab label="Upload Image" />
-        <Tab label="Input_URL" />
-        <Tab label="Input_Image_URL" />
-        <Tab label="Item Two" />
-        <Tab label="Charts" />
+      <Tabs orientation={tabsOrientation} value={value} onChange={handleChange}>
+        <Tab
+          label="Input URL"
+          icon={
+            <Icon fontSize="small" sx={{ mt: -0.25 }}>
+              input
+            </Icon>
+          }
+        />
+        <Tab
+          label="Upload Image"
+          icon={
+            <Icon fontSize="small" sx={{ mt: -0.25 }}>
+              upload
+            </Icon>
+          }
+        />
+        <Tab
+          label="Input Image URL"
+          icon={
+            <Icon fontSize="small" sx={{ mt: -0.25 }}>
+              image
+            </Icon>
+          }
+        />
       </Tabs>
 
       {/* Add the content for each tab */}
-      {value === 0 && (
+      {value === 1 && (
         <Box p={3}>
           <h2>Analyze advertisement by uploaded image</h2>
           <ImageUploader />
         </Box>
       )}
-      {value === 1 && (
+      {value === 0 && (
         <Box p={3}>
           <h2>Analyze advertisement by URL</h2>
           <InputURL />
