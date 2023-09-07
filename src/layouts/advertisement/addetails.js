abb6adeaa9c22024952316612528d8f6d1e75645
Chanuka Lakshan Chandra Yapa <118423471+Chanuka-ChandraYapa@users.noreply.github.com>
2023-09-07 23:40:27 +0530

    updated again

--- src/layouts/advertisement/addetails.js
+++ src/layouts/advertisement/addetails.js
@@ -429,10 +429,239 @@ const Addetail = () => {
       // Render details for wedding proposals category
       return (
         <div>
-          <Typography variant="h5" component="h2" gutterBottom>
-            {adDetails.Title} Marriage
-          </Typography>
-          {/* Add specific details for wedding proposals category */}
+          <MDTypography textAlign="center" variant="h1" fontWeight="medium" color="primary">
+            {adDetails.Title}
+          </MDTypography>
+          <MDTypography textAlign="center" variant="h3" fontWeight="regular" color="text">
+            {adDetails.Description}
+          </MDTypography>
+
+          <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
+            <Card sx={{ my: { xs: 2, md: 4 }, p: { xs: 2, md: 3 } }}>
+              <MDBox p={2}>
+                <MDTypography variant="h4" fontWeight="medium" color="primary">
+                  General Information
+                </MDTypography>
+                <MDBox component="ul" display="flex" flexDirection="column" p={1} m={0}>
+                  <MDBox component="li" display="flex" alignItems="center" py={1} mb={1}>
+                    <MDBox
+                      display="flex"
+                      flexDirection="column"
+                      alignItems="flex-start"
+                      justifyContent="center"
+                    >
+                      <MDTypography variant="h5" fontWeight="regular">
+                        Gender
+                      </MDTypography>
+                    </MDBox>
+                    <MDBox ml="auto">
+                      <MDTypography color="dark" variant="subtitle1" fontWeight="medium">
+                        {adDetails.Gender}
+                      </MDTypography>
+                    </MDBox>
+                  </MDBox>
+
+                  <MDBox component="li" display="flex" alignItems="center" py={1} mb={1}>
+                    <MDBox
+                      display="flex"
+                      flexDirection="column"
+                      alignItems="flex-start"
+                      justifyContent="center"
+                    >
+                      <MDTypography variant="h5" fontWeight="regular">
+                        Age
+                      </MDTypography>
+                    </MDBox>
+                    <MDBox ml="auto">
+                      <MDTypography variant="subtitle1" color="dark">
+                        {adDetails.Age}
+                      </MDTypography>
+                    </MDBox>
+                  </MDBox>
+
+                  <MDBox component="li" display="flex" alignItems="center" py={1} mb={1}>
+                    <MDBox
+                      display="flex"
+                      flexDirection="column"
+                      alignItems="flex-start"
+                      justifyContent="center"
+                    >
+                      <MDTypography variant="h5" fontWeight="regular">
+                        Profession
+                      </MDTypography>
+                    </MDBox>
+                    <MDBox ml="auto">
+                      <MDTypography variant="subtitle1" color="dark">
+                        {adDetails.Profession}
+                      </MDTypography>
+                    </MDBox>
+                  </MDBox>
+
+                  <MDBox component="li" display="flex" alignItems="center" py={1} mb={1}>
+                    <MDBox
+                      display="flex"
+                      flexDirection="column"
+                      alignItems="flex-start"
+                      justifyContent="center"
+                    >
+                      <MDTypography variant="h5" fontWeight="regular">
+                        Nationality
+                      </MDTypography>
+                    </MDBox>
+                    <MDBox ml="auto">
+                      <MDTypography variant="subtitle1" color="dark">
+                        {adDetails.Nationality}
+                      </MDTypography>
+                    </MDBox>
+                  </MDBox>
+
+                  <MDBox component="li" display="flex" alignItems="center" py={1} mb={1}>
+                    <MDBox
+                      display="flex"
+                      flexDirection="column"
+                      alignItems="flex-start"
+                      justifyContent="center"
+                    >
+                      <MDTypography variant="h5" fontWeight="regular">
+                        Other Requirements
+                      </MDTypography>
+                    </MDBox>
+                    <MDBox ml="auto">
+                      <MDTypography variant="subtitle1" color="dark">
+                        {adDetails.Special_Requirements}
+                      </MDTypography>
+                    </MDBox>
+                  </MDBox>
+                  <MDBox component="li" display="flex" alignItems="center" py={1} mb={1}>
+                    <MDBox
+                      display="flex"
+                      flexDirection="column"
+                      alignItems="flex-start"
+                      justifyContent="center"
+                    >
+                      <MDTypography variant="h5" fontWeight="regular">
+                        Posted on
+                      </MDTypography>
+                    </MDBox>
+                    <MDBox ml="auto">
+                      <MDTypography variant="subtitle1" color="dark">
+                        {new Date(adDetails.Posted_Date).toLocaleString()}
+                      </MDTypography>
+                    </MDBox>
+                  </MDBox>
+                  <MDBox component="li" display="flex" alignItems="center" py={1} mb={1}>
+                    <MDBox
+                      display="flex"
+                      flexDirection="column"
+                      alignItems="flex-start"
+                      justifyContent="center"
+                    >
+                      <MDTypography variant="h5" fontWeight="regular">
+                        Source
+                      </MDTypography>
+                    </MDBox>
+                    <MDBox ml="auto">
+                      <MDTypography variant="subtitle1" color="dark">
+                        {adDetails.source}
+                      </MDTypography>
+                    </MDBox>
+                  </MDBox>
+                </MDBox>
+                <MDTypography variant="h4" fontWeight="medium" color="primary">
+                  Contact Information
+                </MDTypography>
+                <MDBox component="ul" display="flex" flexDirection="column" p={1} m={0}>
+                  <MDBox component="li" display="flex" alignItems="center" py={1} mb={1}>
+                    <MDBox
+                      display="flex"
+                      flexDirection="column"
+                      alignItems="flex-start"
+                      justifyContent="center"
+                    >
+                      <MDTypography variant="h5" fontWeight="regular">
+                        Phone Numbers
+                      </MDTypography>
+                    </MDBox>
+                    <MDBox ml="auto">
+                      {adDetails.Contact_Info.Phone_Number.map((phoneNumber, index) => (
+                        <MDButton
+                          key={index}
+                          component="a"
+                          href={`tel:${phoneNumber}`}
+                          target="_blank"
+                          rel="noreferrer"
+                          variant="text"
+                          color="primary"
+                        >
+                          <MDTypography variant="h6">{phoneNumber}</MDTypography>
+                        </MDButton>
+                      ))}
+                    </MDBox>
+                  </MDBox>
+                  <MDBox component="li" display="flex" alignItems="center" py={1} mb={1}>
+                    <MDBox
+                      display="flex"
+                      flexDirection="column"
+                      alignItems="flex-start"
+                      justifyContent="center"
+                    >
+                      <MDTypography variant="h5" fontWeight="regular">
+                        Email
+                      </MDTypography>
+                    </MDBox>
+                    <MDBox ml="auto">
+                      <MDButton
+                        component="a"
+                        href={`mailto:${adDetails.Contact_Info.Email}`}
+                        target="_blank"
+                        rel="noreferrer"
+                        variant="text"
+                        color="primary"
+                      >
+                        <MDTypography variant="h6">{adDetails.Contact_Info.Email}</MDTypography>
+                      </MDButton>
+                    </MDBox>
+                  </MDBox>
+                </MDBox>
+
+                <MDTypography variant="h4" fontWeight="medium" color="primary">
+                  Location
+                </MDTypography>
+                <MDBox component="ul" display="flex" flexDirection="column" p={1} m={0}>
+                  <MDBox component="li" display="flex" alignItems="center" py={1} mb={1}>
+                    <MDBox
+                      display="flex"
+                      flexDirection="column"
+                      alignItems="flex-start"
+                      justifyContent="center"
+                    >
+                      <MDTypography variant="h5" fontWeight="regular">
+                        Nearest City
+                      </MDTypography>
+                    </MDBox>
+                    <MDBox ml="auto">
+                      <MDTypography variant="h5" color="dark" fontWeight="regular">
+                        {adDetails.Location.City}
+                      </MDTypography>
+                    </MDBox>
+                  </MDBox>
+
+                  <MDBox component="li" display="flex" alignItems="center" py={1} mb={1}>
+                    <MDBox
+                      display="flex"
+                      flexDirection="column"
+                      alignItems="flex-start"
+                      justifyContent="center"
+                    >
+                      <MDButton color="primary" onClick={redirectToGoogleMaps}>
+                        Open in Google Maps
+                      </MDButton>
+                    </MDBox>
+                  </MDBox>
+                </MDBox>
+              </MDBox>
+            </Card>
+          </Container>
         </div>
       );
     } else {
